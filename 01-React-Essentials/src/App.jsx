bfe3336ba828395210ae303a398648c7225925f0
Wakabi Brian <wakabibrian95@gmail.com>
2024-05-15 06:54:38 +0300

    Finished React essentials

--- 01-React-Essentials/src/App.jsx
+++ 01-React-Essentials/src/App.jsx
@@ -13,20 +13,23 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       <Header />
       <main>
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept key={conceptItem.title} {...conceptItem} />
+            ))}
+            {/* <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
           </ul>
         </section>
         <section id="examples">
@@ -70,7 +73,7 @@ function App() {
           )}
         </section>
       </main>
-    </div>
+    </>
   );
 }
 
