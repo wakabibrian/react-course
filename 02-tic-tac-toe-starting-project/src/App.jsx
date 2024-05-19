ada3bd7cfe5997bb54d7e00e384499ab562c6fc1
Wakabi Brian <wakabibrian95@gmail.com>
2024-05-20 01:16:26 +0300

    Finished Reduction of state

--- 02-tic-tac-toe-starting-project/src/App.jsx
+++ 02-tic-tac-toe-starting-project/src/App.jsx
@@ -10,15 +10,16 @@ function App() {
 
   function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
+
     setGameTurns((prevTurns) => {
       let currentPlayer = "X";
 
       if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "0";
+        currentPlayer = "O";
       }
 
       const updatedTurns = [
-        { square: { row: rowIndex, col: colIndex }, player: activePlayer },
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
         ...prevTurns,
       ];
 
@@ -43,7 +44,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
